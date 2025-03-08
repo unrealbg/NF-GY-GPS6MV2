9af10c3a7ea65df25bffc3819beb46863420be36
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2025-03-08 17:06:58 +0200

    Add project files.

---
+++ NF_GY-GPS6MV2/GpsData.cs
@@ -0,0 +1,19 @@
+﻿namespace NF_GY_GPS6MV2
+{
+    public class GpsData
+    {
+        public string Time;
+
+        public double Latitude;
+
+        public double Longitude;
+
+        public string FixQuality;
+
+        public string NumSatellites;
+
+        public string HDOP;
+
+        public string Altitude;
+    }
+}
