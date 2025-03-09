425e1a50ac49efd0eaae36ea73ad4ef31630d1c4
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2025-03-09 20:26:53 +0200

    Add AppSettings class for configuration constants
    Introduced a new static class `AppSettings` in the `NF_GY_GPS6MV2` namespace. This class includes public constant string fields for essential configuration settings such as MQTT broker address, topic, client ID, WiFi SSID, WiFi password, and MQTT credentials (username and password).

---
+++ NF_GY-GPS6MV2/AppSettings.cs
@@ -0,0 +1,18 @@
+﻿namespace NF_GY_GPS6MV2
+{
+    public static class AppSettings
+    {
+        // Broker settings
+        public const string MqttBrokerAddress = "Broker Address";
+        public const string MqttTopic = "gps/coords";
+        public const string MqttClientId = "MyGpsClient";
+
+        // WiFi settings
+        public const string WifiSsid = "SSID";
+        public const string WifiPassword = "Pass";
+
+        // MQTT credentials
+        public const string MqttUser = "mqttUser";
+        public const string MqttPassword = "mqttPass";
+    }
+}
