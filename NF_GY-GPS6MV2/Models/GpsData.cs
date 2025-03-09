9ddf6559eebcde98af81ee541d231742e5a08b6d
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2025-03-09 20:26:03 +0200

    Add GpsData class for GPS data representation
    Introduced a new `GpsData` class in the `NF_GY_GPS6MV2.Models` namespace. This class includes properties for `Time`, `Latitude`, `Longitude`, `FixQuality`, `NumSatellites`, `HDOP`, and `Altitude`, facilitating the handling of GPS-related information.

---
+++ NF_GY-GPS6MV2/Models/GpsData.cs
@@ -0,0 +1,19 @@
+﻿namespace NF_GY_GPS6MV2.Models
+{
+    public class GpsData
+    {
+        public string Time { get; set; }
+
+        public double Latitude { get; set; }
+
+        public double Longitude { get; set; }
+
+        public string FixQuality { get; set; }
+
+        public string NumSatellites { get; set; }
+
+        public string HDOP { get; set; }
+
+        public string Altitude { get; set; }
+    }
+}
