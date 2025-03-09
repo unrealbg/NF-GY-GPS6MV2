998711bc4322eacbcf80054cbd40b6eec4607cc0
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2025-03-09 20:26:44 +0200

    Add MqttService class for MQTT connection management
    Introduces the `MqttService` class in the `NF_GY_GPS6MV2.Services` namespace. This class manages MQTT connections and messaging, featuring a private `_client` field, an `IsConnected` property, a constructor for initialization with broker details, and a `Publish` method for sending messages. Includes error handling and console logging for connection status and publishing results.

---
+++ NF_GY-GPS6MV2/Services/MqttService.cs
@@ -0,0 +1,66 @@
+﻿namespace NF_GY_GPS6MV2.Services
+{
+    using System;
+    using System.Text;
+
+    using nanoFramework.M2Mqtt;
+
+    public class MqttService
+    {
+        private MqttClient _client;
+
+        public bool IsConnected => _client != null && _client.IsConnected;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MqttService"/> class.
+        /// </summary>
+        /// <param name="brokerAddress"></param>
+        /// <param name="clientId"></param>
+        /// <param name="user"></param>
+        /// <param name="pass"></param>
+        public MqttService(string brokerAddress, string clientId, string user, string pass)
+        {
+            try
+            {
+                _client = new MqttClient(brokerAddress);
+                _client.Connect(clientId, user, pass);
+
+                if (_client.IsConnected)
+                {
+                    Console.WriteLine("Connected to MQTT broker: " + brokerAddress);
+                }
+                else
+                {
+                    Console.WriteLine("MQTT connection failed");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error initializing MQTT: " + ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Publishes a message to the specified topic.
+        /// </summary>
+        /// <param name="topic"></param>
+        /// <param name="message"></param>
+        public void Publish(string topic, string message)
+        {
+            if (!this.IsConnected)
+            {
+                Console.WriteLine("MQTT client not connected");
+                return;
+            }
+            try
+            {
+                _client.Publish(topic, Encoding.UTF8.GetBytes(message));
+                Console.WriteLine($"Published to {topic}: {message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error publishing to MQTT: " + ex.Message);
+            }
+        }
+    }
+}
