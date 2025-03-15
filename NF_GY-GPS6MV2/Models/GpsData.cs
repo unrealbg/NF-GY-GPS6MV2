22b37f6f381d963f8f48c94843ddf9b35b07074e
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2025-03-15 20:39:48 +0200

    Add SpeedKmh and Date properties to GpsData class
    Updated the `GpsData` class in the `NF_GY_GPS6MV2.Models` namespace to include two new properties: `SpeedKmh` (double) for storing speed in kilometers per hour, and `Date` (DateTime) initialized to the current UTC time. This enhances the functionality of the class for GPS data capture.

--- NF_GY-GPS6MV2/Models/GpsData.cs
+++ NF_GY-GPS6MV2/Models/GpsData.cs
@@ -1,5 +1,7 @@
 ﻿namespace NF_GY_GPS6MV2.Models
 {
+    using System;
+
     public class GpsData
     {
         public string Time { get; set; }
@@ -15,5 +17,9 @@
         public string HDOP { get; set; }
 
         public string Altitude { get; set; }
+
+        public double SpeedKmh { get; set; }
+
+        public DateTime Date { get; set; } = DateTime.UtcNow;
     }
 }
